cba2d364f8dc82889639380f20b7509874c9371a
komali <komalipandillapalli1993@gmail.com>
2025-03-30 00:13:05 +0530

    first time pushing my project to git

---
+++ controllers/firmController.js
@@ -0,0 +1,57 @@
+const Vendor = require('../models/Vendor');
+const Firm = require('../models/Firm')
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination:function(req, file, cb){
+        cb(null, 'uploads/')
+    },
+    filename:function(req, file, cb){
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+})
+
+const upload = multer({storage:storage})
+
+const addFirm = async(req, res) =>{
+    try {
+        const {firmName, area, category, region, offer} = req.body; 
+        const image = req.file? req.file.filename : undefined ;
+
+        const vendor = await Vendor.findById(req.vendorId);
+        if(!vendor){
+            return res.status(404).json({message:'Vendor not found'})
+        }
+
+        const firm =new Firm({firmName, area, category, region, offer, image, vendor:vendor._id})
+        
+        const savedFirm=await firm.save();
+        vendor.firm.push(savedFirm);
+
+        await vendor.save();
+
+        return res.status(200).json({message: 'firm added successfully'})
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({message:'internal server error'})
+    }
+
+}
+
+const deleteFirmById =async (req, res) => {
+    try {
+        const firmId = req.params.firmId;
+        const deleteFirm=await Firm.findByIdAndDelete(firmId);
+
+        if (!deleteFirm){
+            return res.status(404).json({message:"no product found"})
+        }
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({error:"internal server error"})
+    }
+    
+}
+
+module.exports = {addFirm:[upload.single('image'), addFirm], deleteFirmById}
