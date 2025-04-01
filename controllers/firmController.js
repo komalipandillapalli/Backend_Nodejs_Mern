21dff29e142fb6d6ca2f711eeace8293f2cfb338
komali <komalipandillapalli1993@gmail.com>
2025-04-01 22:50:52 +0530

    require(path) is applied to controllers and cors module is installed in index.js

--- controllers/firmController.js
+++ controllers/firmController.js
@@ -1,6 +1,7 @@
 const Vendor = require('../models/Vendor');
 const Firm = require('../models/Firm')
 const multer = require('multer');
+const path = require('path');
 
 const storage = multer.diskStorage({
     destination:function(req, file, cb){
